e7a6a083f4bbc35f8fb75ce23d027aff659e254f
soon15 <162524813+soon15@users.noreply.github.com>
2024-11-15 15:48:56 +0900

    Add files via upload

--- RemoteControl.java
+++ RemoteControl.java
@@ -1,12 +1,5 @@
-package ch08.sec04;
+package ch08.sec02;
 
 public interface RemoteControl {
-	
-	int MAX_VOLUME = 10;
-	int MIN_VOLUME = 0;
-	
-	void turnOn();
-	void turnOff();
-	void setVolume(int volume);
-
+	public void turnOn();
 }
